ef94feea2381a07a5acdb3bad392285fced10779
Kiran <kiranjolisa@gmail.com>
2021-03-09 18:55:11 +0530

    Minor navbar developed for investor relations page

--- investorRelations/investors.js
+++ investorRelations/investors.js
@@ -1,1 +1,11 @@
 import * as data from './data.js'; 
+var minorNavbar = document.getElementById("minorNavBar");
+var setAndUnsetStickyNavbar = () =>{
+    if (window.pageYOffset >= minorNavbar.offsetTop) {
+        minorNavbar.classList.add("sticky")
+      } else {
+        minorNavbar.classList.remove("sticky");
+      }
+}
+window.onscroll = function() {setAndUnsetStickyNavbar()};
+ 
