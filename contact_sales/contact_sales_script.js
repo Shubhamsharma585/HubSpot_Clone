6d84cc7855fa7234daf3dd94d5f03577be58f0de
info-vivekranjan <75258584+info-vivekranjan@users.noreply.github.com>
2021-03-12 14:28:34 +0530

    metting form completed in contact sales

---
+++ contact_sales/contact_sales_script.js
@@ -0,0 +1,5 @@
+function showMeetingData() {
+    window.location = "meeting_index.html"
+}
+var meeting_btn = document.querySelector("#contact-sales-btn3")
+meeting_btn.addEventListener("click", showMeetingData)
